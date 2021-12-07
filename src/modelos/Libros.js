2c14529b7291c13040b46b70c786878510447c0b
Diego Gomez <da.gomezmartinez@ugto.mx>
2021-12-06 20:11:43 -0600

    Se agrego una estructura temporal para los libros para probar Mongo

---
+++ src/modelos/Libros.js
@@ -0,0 +1,14 @@
+const { Schema, model, Model } = require('mongoose');
+
+const libroEstrutura = new Schema({
+    titulo : {type : String},
+    descripcion : {type : String},
+    filename : {type : String},
+    path : {type : String},
+    originalname : {type : String},
+    mimetype : {type : String},
+    size : {type : Number},
+    creandoEl : {type : Date, default: Date.now()}
+});
+
+module.exports = model('Libro', libroEstrutura);
