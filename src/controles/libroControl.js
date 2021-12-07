1b920c2838b2635b1da093814e036c3651d4b437
Diego Gomez <da.gomezmartinez@ugto.mx>
2021-12-06 21:19:23 -0600

    Se creo la funcion para mandar libros a la base de datos. Funcional

--- src/controles/libroControl.js
+++ src/controles/libroControl.js
@@ -1,11 +1,23 @@
 const libroCtrl = {};
+const Libro = require('../modelos/Libros');
 
 libroCtrl.renderAgregarLibro = (req, res) => {
     res.render('libros/nuevoLibro')
 }
-libroCtrl.agregarLibro = (req, res) => {
-    console.log(req.file);
-    res.send('Añadido')
+libroCtrl.agregarLibro = async (req, res) => { 
+    const { titulo, descripcion } = req.body;
+    const { filename, originalname, mimetype, size} = req.file;
+    const libro = new Libro({
+        titulo,
+        descripcion,
+        filename,
+        path : 'img/subidas/' + filename,
+        originalname,
+        mimetype,
+        size
+    });
+    await libro.save();
+    res.redirect('/');
 }
 
 module.exports = libroCtrl;
