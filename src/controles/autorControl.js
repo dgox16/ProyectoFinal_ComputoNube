500dbad70a3091cb9ad557588688c6524c024841
Diego Gomez <da.gomezmartinez@ugto.mx>
2021-12-10 11:53:34 -0600

    Se arreglo para poder editar el nombre de autor y se cambian automaticamente el nombre del autor de todos los libros intervenidos

--- src/controles/autorControl.js
+++ src/controles/autorControl.js
@@ -13,16 +13,19 @@ autorCtrl.renderAdministrarAutor = async (req, res) => {
 }
 
 autorCtrl.administrarAutor = async (req, res) => {
-    const { añoNacimiento, Nacionalidad, frase} = req.body;
+    const autorAux = await Autor.findById(req.params.id);
+    const { nombre, añoNacimiento, Nacionalidad, frase} = req.body;
     const { filename } = req.file;
+    await Libro.updateMany({autor: autorAux.nombre}, {autor: nombre})
     await Autor.findByIdAndUpdate(req.params.id, {
+        nombre,
         añoNacimiento,
         Nacionalidad,
         frase,
         filename,
         path: 'img/subidas/' + filename
     });
-
+    
     res.send('Editado correctamente')
 }
 
