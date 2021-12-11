5a48b5a73ecfbf61f45a85256ce42e7e6f219e3b
Diego Gomez <da.gomezmartinez@ugto.mx>
2021-12-10 20:39:51 -0600

    Primer fase de diseño

--- src/rutas/autor.ruta.js
+++ src/rutas/autor.ruta.js
@@ -4,11 +4,11 @@ const router = Router();
 const { estaAutenticado } = require('../auxiliar/validacionSesion');
 const { renderVistaAutores, renderAdministrarAutor, administrarAutor } = require('../controles/autorControl');
 
-router.get('/autores',  renderVistaAutores);
+router.get('/autores', estaAutenticado,  renderVistaAutores);
 
 
-router.get('/autores/administrar/:id', renderAdministrarAutor);
-router.put('/autores/administrar/:id', administrarAutor);
+router.get('/autores/administrar/:id', estaAutenticado, renderAdministrarAutor);
+router.put('/autores/administrar/:id', estaAutenticado, administrarAutor);
 
 
 module.exports = router;
